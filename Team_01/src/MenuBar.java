19b4f2b39db59d40cacd6a9bd7e5627382a76707
rishikabera <55071483+rishikabera@users.noreply.github.com>
2020-02-19 20:40:20 -0700

    Menu Bar file pushed

---
+++ Team_01/src/MenuBar.java
@@ -0,0 +1,25 @@
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MenuBar extends JMenuBar {
+
+	static JMenu menu;
+	static JMenuItem load, save;
+	
+	MenuBar() {
+		menu = new JMenu("Menu");
+		SaveFile saveFile = new SaveFile("Save");
+		save = saveFile;
+		save.addActionListener(saveFile);
+		LoadFile loadFile = new LoadFile("Load");
+		load = loadFile;
+		load.addActionListener(loadFile);
+		menu.add(save);
+		menu.add(load);
+		this.add(menu);
+		
+		
+	}
+
+}
