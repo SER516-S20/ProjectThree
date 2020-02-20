df9a7d1aac2f505840f4de44a838473d52c15db1
Sarvansh Prasher <sarvanshp@gmail.com>
2020-02-20 15:51:14 -0700

    Added files for accomodaitng square bars functionality

--- Team_02/src/PanelSquare.java
+++ Team_02/src/PanelSquare.java
@@ -16,7 +16,46 @@ import javax.swing.border.EmptyBorder;
  * @author Suryadeep
  * @modified on 01-28-2020
  * @version 2.0
+ * @author Sarvansh Prasher
+ * @modified on 17th Feb 2020
+ * @version 3.0
  */
 public class PanelSquare extends JPanel {
 
+	private static final long serialVersionUID = 1L;
+
+	public PanelSquare() {
+
+		addMouseListener(new PanelMouseListener());
+		JPanel objSquarePane = new JPanel();
+		repaint();
+		objSquarePane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		objSquarePane.setLayout(null);
+	}
+
+	@Override
+	public void paintComponent(Graphics objGraphics) {
+
+		try {
+			Graphics2D obj2D = (Graphics2D) objGraphics;
+			Shape objSquare = new Rectangle(105, 60, 80, 80);
+			Shape bar = new Line2D.Double(110, 70, 110, 130);
+			Shape secondBar = new Line2D.Double(180, 70, 180, 130);
+			obj2D.draw(objSquare);
+			obj2D.draw(bar);
+			obj2D.draw(secondBar);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	private class PanelMouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+
+			ClickedShape objClickedShape = new ClickedShape();
+			objClickedShape.returnShape("Square");
+		}
+	}
 }
--- Team_02/src/ShapeLocation.java
+++ Team_02/src/ShapeLocation.java
@@ -1,6 +1,18 @@
+/**
+* @author Sarvansh Prasher
+* @created on 18th Feb 2020
+* @version 1.0
+*/
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShapeLocation {
+
+	public static List<Point> circlePoint = new ArrayList<Point>();
+	public static List<Point> trianglePoint = new ArrayList<Point>();
+	public static List<Point> squarePoint = new ArrayList<Point>();
+	public static List<Point> pointsPoint = new ArrayList<Point>();
+	public static List<Point> squarebarpoints = new ArrayList<Point>();
+	public static List<Lineconnection> LinePoint = new ArrayList<Lineconnection>();
 }
