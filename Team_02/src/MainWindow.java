c74f828794f7e9642d1ea0578fefb810d7676821
ksharm38 <54971135+ksharm38@users.noreply.github.com>
2020-02-20 15:45:47 -0700

    Implemented menu addition on the main window

--- Team_02/src/MainWindow.java
+++ Team_02/src/MainWindow.java
@@ -6,5 +6,41 @@ import javax.swing.*;
  * @version 1.0
  */
 public class MainWindow {
-
+    java.util.List<Point> displayList = new ArrayList<Point>();
+    String pathname = "data.dat";
+      JButton clearBtn = new JButton("Clear");
+  JButton saveBtn = new JButton("Save");
+  JButton restoreBtn = new JButton("Restore");
+  JButton quitBtn = new JButton("Quit");
+  
+   
+    
+    public static void main(String[] args)
+    {
+       
+    
+        Menu objMenu = new Menu();
+        JFrame frame = new JFrame("Main Window");
+        //JPanel frame = new JPanel();
+        frame.setVisible(true);
+        frame.setSize(1000,1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel panel = new JPanel();
+        panel.add(objMenu.CreateMenu());
+        
+        JSplitPane splitPane2 = new JSplitPane();
+        splitPane2.setSize(900, 900);
+        splitPane2.setDividerSize(0);
+        splitPane2.setDividerLocation(200);
+        splitPane2.setOrientation(JSplitPane.VERTICAL_SPLIT);
+      
+        splitPane2.setTopComponent(new PanelToolkit());
+        splitPane2.setBottomComponent(new DrawBoardPanel());
+        JSplitPane splitPane1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT,panel,splitPane2);      
+        frame.add(splitPane1);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        
+        
+    }
 }
