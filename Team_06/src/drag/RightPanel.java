1e80493ebcb9252edd9ef24c6d599ca1c04dd781
jainrahul1369 <60165897+jainrahul1369@users.noreply.github.com>
2020-02-20 11:43:41 -0800

    Update RightPanel.java

--- Team_06/src/drag/RightPanel.java
+++ Team_06/src/drag/RightPanel.java
@@ -1,5 +1,8 @@
 package drag;
-
+import java.io.*;
+import javax.swing.JFileChooser;
+import java.awt.Component;
+import javax.swing.JOptionPane;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -7,6 +10,7 @@ import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.io.FileWriter;
 
 import javax.swing.JButton;
 
@@ -28,14 +32,15 @@ public class RightPanel extends PanelInterface {
 		return single_instance;
 	}
 
-	private ArrayList<Point> points;
-	ArrayList<Point> circlePoints = new ArrayList<Point>();
-	ArrayList<Point> squarePoints = new ArrayList<Point>();
-	ArrayList<Point> trianglePoints = new ArrayList<Point>();
+	public ArrayList<Point> points;
+	public ArrayList<Point> circlePoints = new ArrayList<Point>();
+	public ArrayList<Point> squarePoints = new ArrayList<Point>();
+	public ArrayList<Point> trianglePoints = new ArrayList<Point>();
 
 	public RightPanel() {
 
 		super("#666666", 150, 20);
+		this.circlePoints = new ArrayList<Point>();
 		System.out.println("Inside Left Panel");
 		points = new ArrayList<Point>();
 		/*
@@ -56,7 +61,9 @@ public class RightPanel extends PanelInterface {
 					JButton circle = new Circle("", e.getX(), e.getY(), true);
 					Object rp = e.getComponent();
 					//System.out.println("rp " + rp);
-					((PanelInterface) rp).add(circle);
+					((RightPanel) rp).add(circle);
+					RightPanel.getInstance().circlePoints.add(new Point( e.getX(), e.getY()));
+					
 					repaint();
 				}
 				else if (finalFlag == 2) {
@@ -64,6 +71,7 @@ public class RightPanel extends PanelInterface {
 					JButton square = new Square("", e.getX(), e.getY(), true);
 					Object rp = e.getComponent();
 					((PanelInterface) rp).add(square);
+					RightPanel.getInstance().squarePoints.add(new Point( e.getX(), e.getY()));
 					repaint();
 				}
 				else if(finalFlag == 3) {	        	
@@ -71,6 +79,7 @@ public class RightPanel extends PanelInterface {
 	        		JButton triangle = new Triangle("", e.getX(), e.getY(), true);
 	          		Object rp = e.getComponent();
 	          		((PanelInterface) rp).add(triangle); 
+	          		RightPanel.getInstance().trianglePoints.add(new Point( e.getX(), e.getY()));
 	          		repaint();
 	     	    }
 
@@ -90,22 +99,22 @@ public class RightPanel extends PanelInterface {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		//int temp = points.size() - 1;
-		//finalFlag = RightPanel.getInstance().finalFlag;
-		/*
-		 * if (finalFlag == 1) { circlePoints.add(new Point(points.get(temp).x,
-		 * points.get(temp).y)); System.out.println(circlePoints); } else if (finalFlag
-		 * == 2) { squarePoints.add(new Point(points.get(temp).x, points.get(temp).y));
-		 * } else if (finalFlag == 3) { trianglePoints.add(new Point(points.get(temp).x,
-		 * points.get(temp).y)); }
-		 * 
-		 * for (Point point : circlePoints) { //
-		 * System.out.println("Inside Left Panel"); JButton circle = new
-		 * Circle("Circle", 300, 300); RightPanel.getInstance().add(circle); //
-		 * RightPanel.getInstance().add(Box.createVerticalStrut(100)); //
-		 * g.drawOval(point.x, point.y, 70, 70); } for (Point point : squarePoints) {
-		 * g.drawRect(point.x, point.y, 70, 70); } for (Point point : trianglePoints) {
-		 * g.drawPolygon(new int[] { point.x, point.x - 30, point.x + 30 }, new int[] {
-		 * point.y, point.y + 70, point.y + 70 }, 3); }
-		 */	}
+		int temp = points.size() - 1;
+		finalFlag = RightPanel.getInstance().finalFlag;
+		
+		  if (finalFlag == 1) 
+		  { 
+			  //circlePoints.add(new Point(points.get(temp).x,points.get(temp).y)); 
+			  System.out.println(circlePoints); 
+		  } 
+		  else if (finalFlag== 2) 
+		  { 
+			  squarePoints.add(new Point(points.get(temp).x, points.get(temp).y));
+		  } 
+		  else if (finalFlag == 3) 
+		  { 
+			  trianglePoints.add(new Point(points.get(temp).x,points.get(temp).y)); 
+		  }
+		  
+}
 }
