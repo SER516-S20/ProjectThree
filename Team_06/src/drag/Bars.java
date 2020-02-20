7c22a65f9afc3ea7c47c624543b164ef4961164b
jainrahul1369 <60165897+jainrahul1369@users.noreply.github.com>
2020-02-20 12:19:34 -0800

    Update Bars.java

--- Team_06/src/drag/Bars.java
+++ Team_06/src/drag/Bars.java
@@ -1,6 +1,9 @@
 package drag;
 
-
+/**
+ * @author Sheran, Somesh
+ * @since 02-19-2019
+ */
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -13,12 +16,8 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.border.EmptyBorder;
 
-/**
- * @author Sheran, Somesh
- * @since 02-19-2019
- */
-
-public class Bars extends JButton{
+public class Bars extends JButton
+{
     public static int endx = 0;
     public static int endy = 0;
     private volatile int myX = 0;
@@ -27,27 +26,34 @@ public class Bars extends JButton{
     public static int starty = 0;
     public static int lineflag = 0;
 
-    public Bars(String label, int x, int y, boolean flag) {
-
+    public Bars(String label, int x, int y, boolean flag) 
+    {
         super(label);
         Dimension size = getPreferredSize();
         setPreferredSize(size);
         setBounds(x, y, 10, 55);
         setBorder(new EmptyBorder(0, 0, 55, 10));
-
         setContentAreaFilled(false);
     }
+    
     @Override
-    protected void paintComponent(Graphics g) {
-        if (getModel().isArmed()) {
+    protected void paintComponent(Graphics g) 
+    {
+        if (getModel().isArmed()) 
+        {
             g.setColor(Color.lightGray);
-        } else {
+        } 
+        else 
+        {
             g.setColor(Color.BLUE);
         }
+        
         g.fillRect(0, 0, getSize().width - 1, getSize().height - 1);
         super.paintComponent(g);
     }
-    protected void paintBorder(Graphics g) {
+    
+    protected void paintBorder(Graphics g) 
+    {
         g.setColor(Color.BLACK);
         g.drawRect(0, 0, getSize().width-1, getSize().height-1);
     }
