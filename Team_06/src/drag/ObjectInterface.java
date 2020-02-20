a389d07d53d9a742494321aa9d44af34a4abb10c
jainrahul1369 <60165897+jainrahul1369@users.noreply.github.com>
2020-02-20 14:44:51 -0800

    Update ObjectInterface.java

--- Team_06/src/drag/ObjectInterface.java
+++ Team_06/src/drag/ObjectInterface.java
@@ -1,29 +1,31 @@
 package drag;
+
 import java.awt.Dimension;
 import javax.swing.*;
 //import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
 /** 
- * @author Sheran
+ * @author Suyog, Dhananjay, Somesh
  * @since 01-29-2019
+ * @Description: This is abstract class for all the panels
  */
-public abstract class ObjectInterface extends JButton {
-	
-	/**
-	 * 
-	 */
+public abstract class ObjectInterface extends JButton 
+{
 	private static final long serialVersionUID = 1L;
-
-	public ObjectInterface(String label, int x, int y) {
+	
+	/** 
+	 * @param x  - x coordinate for the shape
+	 * @param y  - y coordinate for the shape
+	 */
+	public ObjectInterface(String label, int x, int y) 
+	{
         super(label);
         Dimension size = getPreferredSize();
         size.width = size.height = Math.max(size.width,size.height);
         setPreferredSize(size);
         setBounds(x, y, size.width, size.height);
-//        Border emptyBorder = BorderFactory.createEmptyBorder();
-        setBorder(new EmptyBorder(x, y, 5, 5));
-        
+        setBorder(new EmptyBorder(x, y, 5, 5));       
         setContentAreaFilled(false);
     }
 }
