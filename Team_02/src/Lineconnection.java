199526293462f20128d23816d0d2391592527fc6
surya-de <schatt37@asu.edu>
2020-02-20 15:06:07 -0700

    Implemented validation and line draw events

--- Team_02/src/Lineconnection.java
+++ Team_02/src/Lineconnection.java
@@ -1,5 +1,15 @@
 import java.awt.Point;
-
+/**
+ * @author Suryadeep
+ * @created on 01-27-2020
+ * @version 1.0
+ */
 public class Lineconnection {
+	public Point P1 = null;
+	public Point P2 = null;
 
+	Lineconnection(Point P1, Point P2){
+		this.P1 = P1 ;
+		this.P2 = P2;
+	}
 }
--- Team_02/src/PanelCircle.java
+++ Team_02/src/PanelCircle.java
@@ -9,9 +9,43 @@ import javax.swing.border.EmptyBorder;
 
 /**
  * @author Suryadeep
- * @created on 01-27-2020
+ * @created on 02-18-2020
  * @version 1.0
  */
 public class PanelCircle extends JPanel {
+	private static final long serialVersionUID = 1L;
 
+	public PanelCircle() {
+
+		addMouseListener(new PanelMouseListener());
+		JPanel objCirclePane = new JPanel();
+		repaint();
+		objCirclePane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		objCirclePane.setLayout(null);
+	}
+
+	@Override
+	public void paintComponent(Graphics objGraphics) {
+
+		try {
+			Graphics2D obj2D = (Graphics2D) objGraphics;
+			Shape objCircle = new Ellipse2D.Double(105, 60, 80, 80);
+			obj2D.draw(objCircle);
+			obj2D.fillOval(105+40-3, 60+40-3, 6, 6);
+			//Connections c = new Connections();
+			//c.DotToBarConnection(obj2D, 100, 120, 180, 200);
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	private class PanelMouseListener extends MouseAdapter {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+
+			ClickedShape objClickedShape = new ClickedShape();
+			objClickedShape.returnShape("Circle");
+		}
+	}
 }
