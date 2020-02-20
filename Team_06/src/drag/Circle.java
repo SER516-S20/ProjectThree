0039aa21e152e511e33db85f1377d4f8cde4ad4d
ddharne <ddharne@users.github.com>
2020-02-20 15:45:13 -0700

    Code Review and Changes

--- Team_06/src/drag/Circle.java
+++ Team_06/src/drag/Circle.java
@@ -1,25 +1,29 @@
 package drag;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 /** 
- * @author Somesh
+ * @author Rahul, Sheran
  * @since 01-29-2019
- */
-public class Circle extends ObjectInterface {
-	
-	/**
-	 * 
-	 */
+ * @Description: The class creates a button component with circle.
+ */ 
+public class Circle extends ObjectInterface 
+{
 	private static final long serialVersionUID = 1L;
 	private volatile int screenX = 0;
 	private volatile int screenY = 0;
 	private volatile int myX = 0;
 	private volatile int myY = 0;
-
-	public Circle(String label, int x, int y, boolean flag) {   
+	
+	/**
+	 * @param x - x coordinate for circle.
+	 * @param y - y coordinate for circle.
+	 */
+	public Circle(String label, int x, int y, boolean flag) 
+	{   
 		super(label, x, y);
 		
 		Dot dot = new Dot("",33,33,true);
@@ -27,18 +31,19 @@ public class Circle extends ObjectInterface {
 		
 		repaint();
 		
-		if (flag) {
-			addMouseListener(new MouseListener() {
-
+		if (flag) 
+		{
+			addMouseListener(new MouseListener() 
+			{
 				@Override
 				public void mouseClicked(MouseEvent e) {
 				}
 
 				@Override
-				public void mousePressed(MouseEvent e) {
+				public void mousePressed(MouseEvent e) 
+				{
 					screenX = e.getXOnScreen();
 					screenY = e.getYOnScreen();
-
 					myX = getX();
 					myY = getY();
 				}
@@ -56,8 +61,8 @@ public class Circle extends ObjectInterface {
 				}
 
 			});
-			addMouseMotionListener(new MouseMotionListener() {
-
+			addMouseMotionListener(new MouseMotionListener() 
+			{
 				@Override
 				public void mouseDragged(MouseEvent e) {
 					int deltaX = e.getXOnScreen() - screenX;
@@ -69,26 +74,29 @@ public class Circle extends ObjectInterface {
 				@Override
 				public void mouseMoved(MouseEvent e) {
 				}
-
 			});
-
 		}
 	}
+	
 	@Override
-    protected void paintComponent(Graphics g) {
-        if (getModel().isArmed()) {
+    protected void paintComponent(Graphics g) 
+	{
+        if (getModel().isArmed()) 
+        {
             g.setColor(Color.lightGray);
         } 
-        else {
+        else 
+        {
             g.setColor(getBackground());
         }
+        
         g.fillOval(0, 0, getSize().width-1, getSize().height-1);
         super.paintComponent(g);
     }
 
-    protected void paintBorder(Graphics g) {
+    protected void paintBorder(Graphics g) 
+    {
         g.setColor(getForeground());
         g.drawOval(0, 0, getSize().width-1, getSize().height-1);
     }
-
 }
