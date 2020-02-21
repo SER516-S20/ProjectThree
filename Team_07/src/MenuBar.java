5070bf18c7223775201826bf4385c791caa1dd97
praveen kumar panku <praveenkumarpanku@gmail.com>
2020-02-21 16:45:59 -0700

    update MenuBar.java

--- Team_07/src/MenuBar.java
+++ Team_07/src/MenuBar.java
@@ -20,75 +20,77 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 public class MenuBar{
-	
+
 	RightPanel rightPanel;
 	RightPanel olddata;
 	List<Shapes> oldshapesList = new ArrayList<>();
 	JMenuBar appMenuBar;
-	
+
 	public MenuBar() {
 		appMenuBar = new JMenuBar();
-		
-	    JMenu fileButton = new JMenu("File");
-	    appMenuBar.add(fileButton);
-	    JMenuItem openButton = new JMenuItem("Open");
-	    JMenuItem saveButton = new JMenuItem("Save");
-	    fileButton.add(openButton);
-	    fileButton.add(saveButton);        
 
-	    openButton.addActionListener(new ActionListener(){
+		JMenu fileButton = new JMenu("File");
+		appMenuBar.add(fileButton);
+		JMenuItem openButton = new JMenuItem("Open");
+		JMenuItem saveButton = new JMenuItem("Save");
+		fileButton.add(openButton);
+		fileButton.add(saveButton);        
+
+		openButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("open pressed");
 				open();
 			}
-	    });
-	    saveButton.addActionListener(new ActionListener(){
+		});
+		saveButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("save pressed");
 				save();
 			}
-	    });
+		});
 	}
-	
+
 	public void save()
 	{	
-		
+
 		FileDialog dialog = new FileDialog(MainWindow.mainWindow,"Select file to open");
-	    dialog.setMode(FileDialog.SAVE);
-	    dialog.setVisible(true);
-	    String file= dialog.getFile();
-	    if(file == null)
-	    	return;
+		dialog.setMode(FileDialog.SAVE);
+		dialog.setVisible(true);
+		String file= dialog.getFile();
+		if(file == null)
+			return;
 		FileOutputStream fos;
 		ObjectOutputStream oos;
 		rightPanel.bufferShapesList = RightPanel.shapesList;
 		try {
 			fos = new FileOutputStream(file);
 			oos = new ObjectOutputStream(fos);
-	        oos.writeObject(rightPanel);
-	        oos.close();
-	        fos.close();
-	        JOptionPane.showMessageDialog(MainWindow.mainWindow, "Saved as " + file);
+			oos.writeObject(rightPanel);
+			oos.close();
+			fos.close();
+			JOptionPane.showMessageDialog(MainWindow.mainWindow, "Saved as " + file);
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void open()
 	{	
 		FileDialog dialog = new FileDialog(MainWindow.mainWindow,"Select file to open");
-	    dialog.setMode(FileDialog.LOAD);
-	    dialog.setVisible(true);
-	    String file = dialog.getFile();
-	    System.out.println(file + " chosen.");
-	    
+		dialog.setMode(FileDialog.LOAD);
+		dialog.setVisible(true);
+		String file = dialog.getFile();
+		if(file == null)
+			return;
+		System.out.println(file + " chosen.");
+
 		RightPanel.shapesList.clear();
 		rightPanel.repaint();
-		
+
 		try
 		{
 			FileInputStream fi = new FileInputStream(file);
@@ -98,21 +100,21 @@ public class MenuBar{
 			oldshapesList = olddata.bufferShapesList;
 			oi.close();
 			fi.close();
-			
+
 			for (Shapes s : oldshapesList) {
 				System.out.print(s.getClass().getName() + " ");
 				System.out.println(s.xCoordinate + " " + s.yCoordinate);
 				RightPanel.shapesList.add(s);
-	        }
-			
+			}
+
 			rightPanel.repaint();
-			
+
 		} catch (FileNotFoundException e) {
-		System.out.println("File not found");
+			System.out.println("File not found");
 		} catch (IOException e) {
-		System.out.println("Error initializing stream");
+			System.out.println("Error initializing stream");
 		} catch (ClassNotFoundException e) {
-		e.printStackTrace();
+			e.printStackTrace();
 		}
 	}	
 }
