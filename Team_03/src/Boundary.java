882857b9d51b16487a42f02fef91dab8b1aee935
Srinivasan Sundar <srini@Srinivasans-MacBook-Pro.local>
2020-02-22 12:49:59 -0700

    Reverting changes made in Boundary

--- Team_03/src/Boundary.java
+++ Team_03/src/Boundary.java
@@ -15,49 +15,34 @@ public class Boundary {
 		for (int i = 0; i < DrawingArea.listOfShapes.size(); i++) {
 			Object object = DrawingArea.listOfShapes.get(i);
 			if (object instanceof Square) {
-				return checkSquareBoundary(object, coordinateX, coordinateY);
+				Square square = (Square) object;
+				int squareX = square.getCoordinateX();
+				int squareY = square.getCoordinateY();
+				if (polygonCheck(squareX, squareY, squareX + DrawingArea.SQUARE_WIDTH,
+						squareY + DrawingArea.SQUARE_HEIGHT, coordinateX, coordinateY)) {
+					selectedObject = square;
+					return true;
+				}
 			} else if (object instanceof Circle) {
-				return checkCircleBoundary(object, coordinateX, coordinateY);
+				Circle circle = (Circle) object;
+				int circleX = circle.getCoordinateX();
+				int circleY = circle.getCoordinateY();
+				if (polygonCheck(circleX, circleY, circleX + DrawingArea.CIRCLE_WIDTH,
+						circleY + DrawingArea.CIRCLE_HEIGHT, coordinateX, coordinateY)) {
+					selectedObject = circle;
+					return true;
+				}
 			} else if (object instanceof Triangle) {
-				return checkTriangleBoundary(object, coordinateX, coordinateY);
+				Triangle triangle = (Triangle) object;
+				int triangleX = triangle.getCoordinateX();
+				int triangleY = triangle.getCoordinateY();
+				if (polygonCheck(triangleX - DrawingArea.TRIANGLE_SIZE, triangleY,
+						triangleX + DrawingArea.TRIANGLE_SIZE, triangleY + DrawingArea.TRIANGLE_SIZE, coordinateX,
+						coordinateY)) {
+					selectedObject = triangle;
+					return true;
+				}
 			}
-		}
-		return false;
-	}
-
-	private static boolean checkTriangleBoundary(Object object, int coordinateX, int coordinateY) {
-		Triangle triangle = (Triangle) object;
-		int triangleX = triangle.getCoordinateX();
-		int triangleY = triangle.getCoordinateY();
-		if (polygonCheck(triangleX - DrawingArea.TRIANGLE_SIZE, triangleY,
-				triangleX + DrawingArea.TRIANGLE_SIZE, triangleY + DrawingArea.TRIANGLE_SIZE, coordinateX,
-				coordinateY)) {
-			selectedObject = triangle;
-			return true;
-		}
-		return false;
-	}
-
-	private static boolean checkCircleBoundary(Object object, int coordinateX, int coordinateY) {
-		Circle circle = (Circle) object;
-		int circleX = circle.getCoordinateX();
-		int circleY = circle.getCoordinateY();
-		if (polygonCheck(circleX, circleY, circleX + DrawingArea.CIRCLE_WIDTH,
-				circleY + DrawingArea.CIRCLE_HEIGHT, coordinateX, coordinateY)) {
-			selectedObject = circle;
-			return true;
-		}
-		return false;
-	}
-
-	private static boolean checkSquareBoundary(Object object, int coordinateX, int coordinateY) {
-		Square square = (Square) object;
-		int squareX = square.getCoordinateX();
-		int squareY = square.getCoordinateY();
-		if (polygonCheck(squareX, squareY, squareX + DrawingArea.SQUARE_WIDTH,
-				squareY + DrawingArea.SQUARE_HEIGHT, coordinateX, coordinateY)) {
-			selectedObject = square;
-			return true;
 		}
 		return false;
 	}
