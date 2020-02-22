51527ff1d8df5ed8e60e0c44efd9af624aa7bd80
san1197 <sandya7c@gmail.com>
2020-02-22 00:26:55 -0700

    Added author and version in files

--- Team_08/src/SquareShape.java
+++ Team_08/src/SquareShape.java
@@ -6,6 +6,7 @@ import javafx.scene.paint.Color;
  * @version 1.0
  */
 public class SquareShape extends Shape {
+//This class is used to draw square shape in canvas
     public void draw(GraphicsContext g) {
         g.setFill(color);
         g.fillRect(left, top, width, height);
