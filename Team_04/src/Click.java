efd6c4cbf82b85b04cf7657cfa4834ea65537c81
Rohith_Varma <grohithvarma1998@gmail.com>
2020-02-21 22:45:22 -0700

    code refactor

--- Team_04/src/Click.java
+++ Team_04/src/Click.java
@@ -8,37 +8,22 @@ import java.awt.Point;
  */
 public class Click {
 	private static int numPoints = 0;
-	static int x1;
-	static int x2;
-	static int y1;
-	static int y2;
-	static DrawShape shape1;
-	static DrawShape shape2;
-	static Point p1;
-	static Point p2;
+	private static Point point1;
+	private static Point point2;
 
 	public Click(int x, int y, DrawShape shape) {
 		if (numPoints == 0) {
-			x1 = x;
-			y1 = y;
 			numPoints += 1;
-			shape1 = shape;
-			p1 = new Point(x1, y1);
-			Point relative = new Point(x1 - shape.positionX, y1 - shape.positionY);
-			Point[] shapePoints = { p1, relative };
+			point1 = new Point(x, y);
+			Point relative = new Point(x - shape.positionX, y - shape.positionY);
+			Point[] shapePoints = { point1, relative };
 			shape.connections.add(shapePoints);
 		} else {
-			shape2 = shape;
-			x2 = x;
-			y2 = y;
-
-			p2 = new Point(x2, y2);
-			Point[] points = { p1, p2 };
-
-			Point relative = new Point(x2 - shape.positionX, y2 - shape.positionY);
-			Point[] shapePoints = { p2, relative };
+			point2 = new Point(x, y);
+			Point[] points = { point1, point2 };
+			Point relative = new Point(x - shape.positionX, y - shape.positionY);
+			Point[] shapePoints = { point2, relative };
 			shape.connections.add(shapePoints);
-
 			shape.canvas.lineArray.add(points);
 			shape.canvas.repaint();
 			numPoints = 0;
