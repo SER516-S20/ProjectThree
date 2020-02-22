cc24906a89bd60d52591a6579cc3c16376b3edf9
san1197 <sandya7c@gmail.com>
2020-02-22 00:29:07 -0700

    Comments for readibility

--- Team_08/src/TriangleShapeWithDots.java
+++ Team_08/src/TriangleShapeWithDots.java
@@ -4,8 +4,9 @@ import javafx.scene.canvas.GraphicsContext;
  * @author Amudhan
  * @version 1.0
  */
-public class TriangleShapeWithDots extends Shape {
 
+public class TriangleShapeWithDots extends Shape {
+//This class is used to draw three dots in each triangle
     public void draw(GraphicsContext g) {
         g.setFill(color);
         g.fillPolygon(new double[]{left - 10, left + 70, left - 70}, new double[]{top - 40, top + 40, top + 40}, 3);
